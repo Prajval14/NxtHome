36f92dd65b1164155975da4684153ffd477a11e3
Prajval Patel <prajval1420@gmail.com>
2024-03-31 23:11:19 -0400

    Added planning form page

--- static/js/planning.js
+++ static/js/planning.js
@@ -1,30 +1,71 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("submit_button").addEventListener("click", function() {
-        event.preventDefault();
-        var formData = {};
-        var formElements = document.getElementById("myForm").elements;
-        for (var i = 0; i < formElements.length; i++) {
-            if (formElements[i].tagName === "INPUT" || formElements[i].tagName === "TEXTAREA") {
+    let formPages = $('#plan_form').find('.form-group');
+    let currentIndex = 0;
+    showTab(currentIndex);    
+
+    $('#next_btn').on('click', function() {    
+        currentIndex++;
+        showTab(currentIndex);
+    });
+
+    $('#previous_btn').on('click', function() {                
+        currentIndex--;
+        showTab(currentIndex);
+    });
+
+    function showTab(n) {
+        // debugger
+        formPages.removeClass('active').eq(n).addClass('active');
+        if (n === 0) {
+            $('#previous_btn').prop('disabled', true);
+        } else {
+            $('#previous_btn').prop('disabled', false);
+        }
+        if (n === formPages.length - 1) {
+            // debugger
+            $('#next_btn').text('Submit');
+            $('#next_btn').addClass('submit_button');
+            addClickEventToSubmitButton();
+        } else {
+            $('#next_btn').text('Next');
+        }
+    }
+});
+
+function addClickEventToSubmitButton() {
+    $('.submit_button').on('click', function() {                
+        postFormData();
+    });
+}
+
+function postFormData() {
+    // debugger
+    event.preventDefault();
+    var formData = {};
+    var formElements = document.getElementById('plan_form').elements;
+    for (var i = 0; i < formElements.length; i++) {
+        if (formElements[i].tagName === 'INPUT' || formElements[i].tagName === 'SELECT') {
+            if (formElements[i].type === 'radio') {
+                var selectedOption = document.querySelector('input[name="' + formElements[i].name + '"]:checked');
+                if (selectedOption) {
+                    formData[selectedOption.name] = selectedOption.value;
+                }
+            }
+            else {
                 formData[formElements[i].name] = formElements[i].value;
             }
         }
-        console.log(formData);   
-        const params = {city: formData.city} 
-        const url = '/get_data?' + new URLSearchParams(params);
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.error('Error:', error));
-    });
-});
+    }
+    console.log(formData);   
 
-function fetchData() {
-    return fetch('/get_data' + area)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.error('Error:', error));
+    //Send this data to python backend
+
+    // const params = {city: formData.city} 
+    // const url = '/get_data?' + new URLSearchParams(params);
+    // fetch(url)
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log(data);
+    // })
+    // .catch(error => console.error('Error:', error));
 }
